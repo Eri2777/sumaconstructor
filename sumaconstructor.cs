9c2d4619c6f54c7d3950d28410a8ec0417e2e0e7
Eri2777 <114264153+Eri2777@users.noreply.github.com>
2022-09-23 16:26:25 -0600

    Add files via upload

---
+++ sumaconstructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            suma SUMA = new suma();
+            SUMA.V1 = double.Parse(textBox1.Text);
+            SUMA.V2 = double.Parse(textBox2.Text);
+
+            textBox3.Text = SUMA.sumar().ToString();
+        }
+    }
+}
